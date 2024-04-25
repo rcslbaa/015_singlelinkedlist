9ce2881d9bdfbf6e8f1770e90952a3318a71957f
rcslbaa <ifasmarani3125@gmail.com>
2024-04-25 18:27:19 +0700

    commit 4

--- singlelinkedlist/singlelinkedlist.cpp
+++ singlelinkedlist/singlelinkedlist.cpp
@@ -20,4 +20,13 @@ void addNode() {
 	nodeBaru->noMhs = nim;
 	nodeBaru->name = nama;
 	if (START == NULL || nim <= START->noMhs) {
-		
+		if (START != NULL && nim == START->noMhs)
+		{
+			cout << "NIM sudah ada" << endl;
+			return;
+		}
+
+		nodeBaru->next = START;
+		START = nodeBaru;
+		return;
+	}
