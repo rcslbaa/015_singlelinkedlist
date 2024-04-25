2859a758aa14e0dc06858c168bc719d010454f8e
rcslbaa <ifasmarani3125@gmail.com>
2024-04-25 18:26:51 +0700

    commit 3

--- singlelinkedlist/singlelinkedlist.cpp
+++ singlelinkedlist/singlelinkedlist.cpp
@@ -8,3 +8,16 @@ struct Node {
 };
 
 Node* START = NULL;
+
+void addNode() {
+	int nim;
+	string nama;
+	Node* nodeBaru = new Node();
+	cout << "Masukkan NIM: ";
+	cin >> nim;
+	cout << "Masukkan Nama: ";
+	cin >> nama;
+	nodeBaru->noMhs = nim;
+	nodeBaru->name = nama;
+	if (START == NULL || nim <= START->noMhs) {
+		
