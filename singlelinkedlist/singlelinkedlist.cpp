ae5c46b60c3aea635677f1888fc654d23ebf2540
rcslbaa <ifasmarani3125@gmail.com>
2024-04-25 18:29:14 +0700

    commit 8

--- singlelinkedlist/singlelinkedlist.cpp
+++ singlelinkedlist/singlelinkedlist.cpp
@@ -47,3 +47,12 @@ void addNode() {
 	nodeBaru->next = current;
 	previous->next = nodeBaru;
 }
+
+bool serachNode(int nim, Node* current, Node* previous) {
+	previous = START;
+	current = START;
+	while (current != NULL && nim > current->noMhs)
+	{
+		previous = current;
+		current = current->next;
+	}
